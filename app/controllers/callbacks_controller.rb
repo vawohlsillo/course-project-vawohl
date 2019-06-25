14f6d08da20611aa3b1a4d1f116b72133be778bf
vawohlsillo <vawohl@miuandes.cl>
2019-06-24 23:01:35 -0400

    Check out omniauth

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,23 @@
 class CallbacksController < Devise::OmniauthCallbacksController
 	def google_oauth2
-		@user = User.from_omniauth(request.env["omniauth.auth"]
+		@user = User.from_omniauth(request.env["omniauth.auth"])
 		sign_in_and_redirect @user, notice: 'Successfully log in !'	
+	end
+
+	# GET|POST /resource/auth/twitter
+   	def passthru
+     		super
+   	end
+
+	# GET|POST /users/auth/twitter/callback
+	def failure
+		super
+	end
+
+	protected
+
+	# The path used when OmniAuth fails
+	def after_omniauth_failure_path_for(scope)
+		super(scope)
 	end
 end
